2ed0a1440ffeac6799adf92a4da476c60fc2a850
hylo <hylo@posteo.de>
2025-07-04 10:52:33 +0200

    wip

--- Main.lean
+++ Main.lean
@@ -1,6 +1,7 @@
 import RiseLean
 import Lean
 open Lean Elab Meta
+open Lean.Elab.Term
 
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
@@ -41,9 +42,18 @@ def main : IO Unit :=
 --                    toMem : (δ1 δ2 : data) → δ1 → (δ1 → δ2) → δ2
 --                   mapVec : (n : nat) → (δ1 δ2 : data) → (δ1 → δ2) → n<δ1> → n<δ2>
 --                 asVector : (n m : nat) → (δ : data) → nm.δ → n.m<δ>
---                 asScalar : (n m : nat) → (δ : data) → n.m<δ> → nm.δ
+--                 asScalar : (n m : nat) → (δ : scalar) → n.m<δ, actually scalar> → nm.δ
 
---   δ ::= n.δ | δ × δ | idx [n] | float | n<float>  (Array Type, Pair Type, Index Type, Scalar Type, Vector Type)
+--   δ ::= n.δ | δ × δ | "idx [" n "]" | float | n<float>  (Array Type, Pair Type, Index Type, Scalar Type, Vector Type)
+--vector 2,4,8,16
+-- e.g. 16vfloat, 16_float
+-- idx, range 0 to n excluding
+
+-- https://github.com/rise-lang/shine/blob/main/src/main/scala/rise/core/types/ExprType.scala
+-- https://github.com/rise-lang/shine/blob/main/src/main/scala/rise/core/Expr.scala
+-- example code benchmarks
+
+
 inductive RiseData
   | array  : Nat → RiseData → RiseData
   | pair   : RiseData → RiseData → RiseData
@@ -52,14 +62,14 @@ inductive RiseData
   | vector : Nat → RiseData
 
 declare_syntax_cat rise_data
-syntax:50 num "." rise_data:50       : rise_data -- todo: not num, but nat expr, potentially with identifiers; also: doesn't work without spaces. workaround?
+syntax:50 num "·" rise_data:50       : rise_data -- todo: not num, but nat expr, potentially with identifiers; also: doesn't work without spaces. workaround?
 syntax:50 "float" :          rise_data
 syntax:10 rise_data "×" rise_data : rise_data
 syntax "idx" "[" num "]" :rise_data -- todo: not num, see above
 syntax "(" rise_data ")" : rise_data
 
 partial def elabRiseData : Syntax → MetaM Expr
-  | `(rise_data| $n:num . $d:rise_data) => do
+  | `(rise_data| $n:num · $d:rise_data) => do
     let d ← elabRiseData d
     mkAppM ``RiseData.array  #[mkNatLit n.getNat, d]
   | `(rise_data| float) => mkAppM ``RiseData.scalar #[]
@@ -72,10 +82,10 @@ partial def elabRiseData : Syntax → MetaM Expr
 
 elab "test_elabRiseData " l:rise_data : term => elabRiseData l
 
-#reduce test_elabRiseData 4 . 5 . float
+#reduce test_elabRiseData 4·5·float
 
-#reduce test_elabRiseData 1 . 2 . float × 3 . 4 . float
-#reduce test_elabRiseData (1 . 2 . float) × (3 . 4 . float)
+#reduce test_elabRiseData 1·2·float × 3·4·float
+#reduce test_elabRiseData (1·2·float) × (3·4·float)
 
 
 inductive RiseLit
@@ -91,8 +101,6 @@ declare_syntax_cat rise_lit
 syntax num       : rise_lit
 
 def elabRiseLit : Syntax → MetaM Expr
-  -- `mkAppM` creates an `Expr.app`, given the function `Name` and the args
-  -- `mkNatLit` creates an `Expr` from a `Nat`
   | `(rise_lit| $n:num) => mkAppM ``RiseLit.nat  #[mkNatLit n.getNat]
   | _ => throwUnsupportedSyntax
 
@@ -103,7 +111,9 @@ elab "test_elabRiseLit " l:rise_lit : term => elabRiseLit l
 
 --   e ::= fun(x, e) | e (e) | x | l | P             (Abstraction, Application, Identifier, Literal, Primitives)
 inductive RiseExpr
+-- todo: use lean identifier? check binders again
   | abst  : String → RiseExpr → RiseExpr
+
   | app   : RiseExpr → RiseExpr → RiseExpr
   | ident : String → RiseExpr
   | lit   : RiseLit → RiseExpr
@@ -114,19 +124,13 @@ declare_syntax_cat                         rise_expr
 syntax rise_lit                          : rise_expr
 syntax ident                             : rise_expr
 syntax "fun" "(" ident "," rise_expr ")" : rise_expr
-syntax rise_expr "(" rise_expr ")"       : rise_expr
-syntax rise_expr "|>" rise_expr          : rise_expr
-
+syntax rise_expr "(" rise_expr ")"       : rise_expr -- application
+syntax rise_expr "|>" rise_expr          : rise_expr -- sugar for application
+syntax rise_expr ">>" rise_expr          : rise_expr -- sugar for application
+syntax rise_expr "<<" rise_expr          : rise_expr -- sugar for application
 -- todo: primitives?
 
-
--- todo: couldn't figure out how to apply this macro before elabRiseExpr,
---       so now RiseExpr.app is kinda doubled
-macro_rules
-  | `(rise_expr| $e:rise_expr |> $f:rise_expr) => `(rise_expr| $f($e))
-
-
-partial def elabRiseExpr : Syntax → MetaM Expr
+partial def elabRiseExpr : Syntax → TermElabM Expr
   | `(rise_expr| $l:rise_lit) => do
     let l ← elabRiseLit l
     mkAppM ``RiseExpr.lit #[l]
@@ -138,10 +142,9 @@ partial def elabRiseExpr : Syntax → MetaM Expr
       let e1 ← elabRiseExpr e1
       let e2 ← elabRiseExpr e2
       mkAppM ``RiseExpr.app #[e1, e2]
-  | `(rise_expr| $e1:rise_expr |> $e2:rise_expr) => do
-      let e1 ← elabRiseExpr e1
-      let e2 ← elabRiseExpr e2
-      mkAppM ``RiseExpr.app #[e2, e1]
+  | `(rise_expr| $e:rise_expr |> $f:rise_expr) => do
+    let s ← `(rise_expr| $f($e))
+    elabRiseExpr s
   | _ => throwUnsupportedSyntax
 
 elab "test_elabRiseExpr " e:rise_expr : term => elabRiseExpr e
--- questions.md
+++ questions.md
@@ -1,3 +1,16 @@
 # in `mult : (δ : data) → δ → δ → δ`, is `(delta : data)` implicit?
 
+curly is implicit, round is explicit
+
 # how much should the primitives be part of the actual syntax? do we want to recognize primitives later, and when parsing they are just identifiers? or should they be part of the grammar?
+
+--
+
+--
+
+tuesdays 10:00
+
+15th
+q
+
+type system
