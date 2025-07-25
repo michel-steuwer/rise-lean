1ac04859012b1d140150d7f71388a6cb2b02902e
hylo <hylo@posteo.de>
2025-07-25 11:13:45 +0200

    refactor: cleanup

--- RiseLean/Expr.lean
+++ RiseLean/Expr.lean
@@ -6,20 +6,12 @@ abbrev RTypingCtx := Array (Name × Option RType)
 
 inductive RExpr where
   | bvar (deBruijnIndex : Nat) (userName : String)
-  -- | const (declName : Name) (us : List VLevel)
   | lit (val : Nat)
   | app (fn arg : RExpr)
 
   | lam (body : RExpr) (binderKind : Option RType)
   | ulam (body : RExpr) (binderType : Option RKind)
 deriving Repr
-
--- def dot : RExpr := .lam (.app (.app (.prim "reduce") (.prim "add")) (.lit 0))
--- def s : RExpr := .lam <| .lam (.app (.app
---                                       (.prim "plus")
---                                       (.bvar 0 "a"))
---                                     (.bvar 1 "b"))
-
 
 declare_syntax_cat rise_expr
 syntax num                                                  : rise_expr
@@ -68,80 +60,30 @@ partial def elabToRExpr (tctx : RTypingCtx) (kctx : RKindingCtx) (mctx : MVarCtx
 instance : ToExpr RExpr where
   toExpr :=
     let rec go : RExpr → Expr
-    | RExpr.lit n => 
+    | RExpr.lit n =>
         mkAppN (mkConst ``RExpr.lit) #[mkNatLit n]
-    | RExpr.bvar index name => 
+    | RExpr.bvar index name =>
         mkAppN (mkConst ``RExpr.bvar) #[mkNatLit index, mkStrLit name]
-    | RExpr.lam body tyOpt => 
+    | RExpr.lam body tyOpt =>
         let bodyExpr := go body
         let tyOptExpr := match tyOpt with
           | none => mkApp (mkConst ``Option.none [levelZero]) (mkConst ``RType)
           | some ty => mkAppN (mkConst ``Option.some [levelZero]) #[mkConst ``RType, toExpr ty]
         mkAppN (mkConst ``RExpr.lam) #[bodyExpr, tyOptExpr]
-    | RExpr.ulam body kindOpt => 
+    | RExpr.ulam body kindOpt =>
         let bodyExpr := go body
         let kindOptExpr := match kindOpt with
           | none => mkApp (mkConst ``Option.none [levelZero]) (mkConst ``RKind)
           | some kind => mkAppN (mkConst ``Option.some [levelZero]) #[mkConst ``RKind, toExpr kind]
         mkAppN (mkConst ``RExpr.ulam) #[bodyExpr, kindOptExpr]
-    | RExpr.app e1 e2 => 
+    | RExpr.app e1 e2 =>
         mkAppN (mkConst ``RExpr.app) #[go e1, go e2]
     go
   toTypeExpr := mkConst ``RExpr
 
--- Helper function to convert from the old elaboration style
 def elabRExpr (tctx : RTypingCtx) (kctx : RKindingCtx) (mctx : MVarCtx) (stx : Syntax) : TermElabM Expr := do
   let rexpr ← elabToRExpr tctx kctx mctx stx
   return toExpr rexpr
-
--- partial def elabRExpr (tctx : RTypingCtx) (kctx : RKindingCtx) (mctx : MVarCtx) : Syntax → TermElabM Expr
---   | `(rise_expr| $l:num) => do
---     mkAppM ``RExpr.lit #[mkNatLit l.getNat]
-
---   | `(rise_expr| $i:ident) => do
---     match tctx.reverse.findIdx? (λ (name, _) => name == i.getId) with
---       | some index =>
---         mkAppM ``RExpr.bvar #[mkNatLit <| index, mkStrLit i.getId.toString]
---       -- could give a hint here if we find the identifier in the kinding context.
---       | none => throwErrorAt i s!"unknown identifier {mkConst i.getId}"
-
---   | `(rise_expr| fun $x:ident => $b:rise_expr )
---   | `(rise_expr| fun ($x:ident) => $b:rise_expr ) => do
---     let b ← elabRExpr (tctx.push (x.getId, none)) kctx mctx b
---     let none := mkApp (mkConst ``Option.none [levelZero]) (mkConst ``RType)
---     mkAppM ``RExpr.lam #[b, none]
-
---   | `(rise_expr| fun $x:ident : $t:rise_type => $b:rise_expr )
---   | `(rise_expr| fun ( $x:ident : $t:rise_type ) => $b:rise_expr ) => do
---     let b ← elabRExpr (tctx.push (x.getId, none)) kctx mctx b
---     -- let syntaxTerm : TSyntax `term ← `([RiseT| $t])
---     -- let t ← Term.elabTerm syntaxTerm (some (mkConst ``RType))
---     let t ← elabRType kctx mctx t
---     let some := mkAppN (mkConst ``Option.some [levelZero]) #[mkConst ``RType, t]
---     mkAppM ``RExpr.lam #[b, some]
-
---   | `(rise_expr| fun ( $x:ident : $t:rise_kind ) => $b:rise_expr ) => do
---     let syntaxTerm : TSyntax `term ← `([RiseK| $t])
---     let t ← Term.elabTerm syntaxTerm none --(mkConst ``RKind) --(some (mkConst ``RType))
---     let b ← elabRExpr tctx (kctx.push (x.getId, t)) mctx b
---     let some := mkAppN (mkConst ``Option.some [levelZero]) #[mkConst ``RKind, t]
---     -- let none := mkApp (mkConst ``Option.none [levelZero]) (mkConst ``RType)
---     mkAppM ``RExpr.ulam #[b, some]
-
---   | `(rise_expr| $e1:rise_expr $e2:rise_expr ) => do
---       let e1 ← elabRExpr tctx kctx mctx e1
---       let e2 ← elabRExpr tctx kctx mctx e2
---       mkAppM ``RExpr.app #[e1, e2]
-
---   | `(rise_expr| $e:rise_expr |> $f:rise_expr) => do
---     let s ← `(rise_expr| $f $e)
---     elabRExpr tctx kctx mctx s
-
---   | `(rise_expr| ( $e:rise_expr )) => do
---     let s ← `(rise_expr| $e)
---     elabRExpr tctx kctx mctx s
-
---   | _ => throwUnsupportedSyntax
 
 elab "[RiseE|" e:rise_expr "]" : term => do
   let p ← liftMacroM <| expandMacros e
